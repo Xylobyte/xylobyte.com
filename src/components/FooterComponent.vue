710f3df510c7c227b722d1298212b00b9f911dd4
Nantsa Montillet <nantsamontillet@xylobyte.com>
2025-02-15 22:45:18 +0100

    Fix responsive

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,11 @@
 <template>
 	<footer class="footer flex j-center a-center column gap5">
 		<span class="jura f-medium">© Copyright 2023 - 2025 | Nantsa Montillet</span>
-		<span class="flex row a-center gap5">
+		<span class="legal flex row a-center gap5">
 			<RouterLink class="txt-link jura" to="/mentions-legales">Mentions légales</RouterLink>
-			<span class="jura">
-				| <a class="txt-link" href="https://github.com/Xylobyte/xylobyte.com">Code source</a> sous license
+			<span class="jura flex row gap5">
+				<span class="separator">|</span>
+				<a class="txt-link" href="https://github.com/Xylobyte/xylobyte.com">Code source</a> sous license
 				<a class="txt-link" href="https://www.gnu.org/licenses/agpl-3.0.fr.html">AGPL v3.0</a>
 			</span>
 		</span>
@@ -12,6 +13,8 @@
 </template>
 
 <style lang="scss" scoped>
+	@use '@/assets/styles/global_var';
+
 	footer {
 		width: 100%;
 		height: 10vh;
@@ -25,6 +28,16 @@
 
 		a {
 			color: var(--light-primary-color);
+		}
+
+		@media (max-width: global_var.$mobile-width) {
+			span.legal {
+				flex-direction: column-reverse;
+
+				.separator {
+					display: none;
+				}
+			}
 		}
 	}
 </style>
