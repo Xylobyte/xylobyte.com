af5a6cd755b28475256efa548f75330c22db9a72
Nantsa Montillet <nantsamontillet@xylobyte.com>
2026-01-12 18:17:39 +0100

    fix: host error

--- src/api/XylobyteAPI.ts
+++ src/api/XylobyteAPI.ts
@@ -3,20 +3,19 @@ import type { ContactRequestData } from '@/api/contact.types.ts';
 import type { XylobyteText } from '@/api/texts.types.ts';
 import axios from 'axios';
 
+const host = 'https://api.xylobyte.com';
+
 export class XylobyteAPI {
-	static getHardSkills = async (): Promise<HardSkills> =>
-		await fetchData('http://127.0.0.1:3000/v1/website/hardskills');
+	static getHardSkills = async (): Promise<HardSkills> => await fetchData('/v1/website/hardskills');
 
-	static getSoftSkills = async (): Promise<SoftSkill[]> =>
-		await fetchData('http://127.0.0.1:3000/v1/website/softskills');
+	static getSoftSkills = async (): Promise<SoftSkill[]> => await fetchData('/v1/website/softskills');
 
-	static getLanguagesSkills = async (): Promise<LanguageSkill[]> =>
-		await fetchData('http://127.0.0.1:3000/v1/website/languages-skills');
+	static getLanguagesSkills = async (): Promise<LanguageSkill[]> => await fetchData('/v1/website/languages-skills');
 
-	static getProjects = async (): Promise<Project[]> => await fetchData('http://127.0.0.1:3000/v1/website/projects');
+	static getProjects = async (): Promise<Project[]> => await fetchData('/v1/website/projects');
 
 	static sendContactRequest = async (data: ContactRequestData) => {
-		const response = await fetch('http://127.0.0.1:3000/v1/website/contact', {
+		const response = await fetch('/v1/website/contact', {
 			body: JSON.stringify(data),
 			method: 'POST',
 			cache: 'no-cache',
@@ -36,22 +35,17 @@ export class XylobyteAPI {
 	};
 
 	static getTexts = async (lang: string): Promise<XylobyteText[]> =>
-		await fetchData(`http://127.0.0.1:3000/v1/website/texts?lang=${lang}`);
+		await fetchData(`/v1/website/texts?lang=${lang}`);
 
 	static logAccess = async () => {
-		await axios.post('http://127.0.0.1:3000/v1/admin/access-stats/portfolio');
+		await axios.post(host + '/v1/admin/access-stats/portfolio');
 	};
 }
 
 const fetchData = async (url: string) => {
-	const res = await axios.get(
-		url,
-		import.meta.env.SSR
-			? {
-					adapter: 'http',
-				}
-			: undefined,
-	);
+	const res = await axios.get(host + url, {
+		adapter: import.meta.env.SSR ? 'http' : undefined,
+	});
 	if (res.status !== 200 && res.status !== 201) throw res.statusText;
 	return res.data;
 };
